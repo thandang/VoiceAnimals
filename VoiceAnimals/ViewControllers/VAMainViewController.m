131de5dafa397223e1ac32583786ba05490bda4a
Than Dang <caotangxm@gmail.com>
2015-05-30 12:25:54 +0700

    initial project

--- VoiceAnimals/ViewControllers/VAMainViewController.m
+++ VoiceAnimals/ViewControllers/VAMainViewController.m
@@ -7,8 +7,21 @@
 //
 
 #import "VAMainViewController.h"
+#import <iAd/iAd.h>
+#import "FancyTabBar.h"
+#import "UIView+Screenshot.h"
+#import "UIImage+ImageEffects.h"
 
-@interface VAMainViewController ()
+
+@interface VAMainViewController () <ADBannerViewDelegate, FancyTabBarDelegate> {
+    UIView *_viewInfo;
+    ADBannerView    *_banner;
+    BOOL            _bannerVisible;
+    
+    FancyTabBar *_fancyTabBar;
+}
+
+@property (nonatomic,strong) UIImageView *backgroundView;
 
 @end
 
@@ -24,14 +37,86 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) loadView {
+    [super loadView];
+    
+    _fancyTabBar = [[FancyTabBar alloc]initWithFrame:self.view.bounds];
+    [_fancyTabBar setUpChoices:self choices:@[@"New", @"Camera"] withMainButtonImage:[UIImage imageNamed:@"main_button"]];
+    _fancyTabBar.delegate = self;
+    [self.view addSubview:_fancyTabBar];
 }
-*/
+
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    _banner = [[ADBannerView alloc] initWithFrame:CGRectMake(0.0, self.view.frame.size.height, self.view.frame.size.width, 50.0)];
+    [_banner setDelegate:self];
+}
+
+
+#pragma mark - FancyTabBarDelegate
+- (void) didCollapse{
+    [UIView animateWithDuration:0.3 animations:^{
+        _backgroundView.alpha = 0;
+    } completion:^(BOOL finished) {
+        if(finished) {
+            [_backgroundView removeFromSuperview];
+            _backgroundView = nil;
+        }
+    }];
+}
+
+
+- (void) didExpand{
+    if(!_backgroundView){
+        _backgroundView = [[UIImageView alloc]initWithFrame:self.view.bounds];
+        _backgroundView.alpha = 0;
+        [self.view addSubview:_backgroundView];
+    }
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        _backgroundView.alpha = 1;
+    } completion:^(BOOL finished) {
+    }];
+    
+    [self.view bringSubviewToFront:_fancyTabBar];
+    UIImage *backgroundImage = [self.view convertViewToImage];
+    UIColor *tintColor = [UIColor colorWithWhite:1.0 alpha:0.5];
+    UIImage *image = [backgroundImage applyBlurWithRadius:10 tintColor:tintColor saturationDeltaFactor:1.8 maskImage:nil];
+    _backgroundView.image = image;
+}
+
+- (void)optionsButton:(UIButton*)optionButton didSelectItem:(int)index{
+    if (index == 1) {
+
+    } else if (index == 2) {
+        UIAlertView *alr = [[UIAlertView alloc] initWithTitle:@"Sorry" message:@"This feature are currently develop. \nComming soon" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alr show];
+    }
+}
+
+#pragma mark - ADBanner Delegate
+- (void) bannerViewDidLoadAd:(ADBannerView *)banner {
+    if (!_bannerVisible) {
+        //If banner isn't part of view hierachy, add it
+        if (_banner.superview == nil) {
+            [self.view addSubview:_banner];
+        }
+        [UIView beginAnimations:@"animateAdBannerOn" context:NULL];
+        banner.frame = CGRectOffset(banner.frame, 0, -banner.frame.size.height);
+        [UIView commitAnimations];
+        _bannerVisible = YES;
+    }
+}
+
+- (void) bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error {
+    if (_bannerVisible) {
+        [UIView beginAnimations:@"animateAdBannerOff" context:NULL];
+        banner.frame = CGRectOffset(banner.frame, 0, banner.frame.size.height);
+        [UIView commitAnimations];
+        _bannerVisible = NO;
+    }
+}
+
+
 
 @end
